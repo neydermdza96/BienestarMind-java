0d39ee3f3bead5f927415ec6e45f77f741e79a0e
Cxstiboi <Samuelcastillo1007@gmail.com>
2025-11-13 19:47:30 -0500

    primer commit del proyecto BienestarMind java

---
+++ src/main/java/com/Proyecto/BienestarMind/BienestarMindApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto.BienestarMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BienestarMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BienestarMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto/BienestarMind/BienestarMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.BienestarMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BienestarMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
